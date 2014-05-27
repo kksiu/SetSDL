6bb3591a1e6c41259b66f34024c1a25a031061fc
Alex Kersten <alex@dividebyxero.com>
2014-05-26 20:26:38 -0500

    forgot the file

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,57 @@
 
 //main function
 int main(int argc, char *argv[]) {
+
+	// Start SDL
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		// XXX: Proper cleanup
+		return 1;
+	}
+
+	// Create window
+	SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+	if (win == nullptr) {
+		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+		// XXX: Proper cleanup
+		return 1;
+	}
+
+	// Create renderer
+	SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (ren == nullptr) {
+		std::cout << "SDL_CreatRenderer Error: " << SDL_GetError() << std::endl;
+		// XXX: Proper cleanup
+		return 1;
+	}
+
+	// Create surface
+	SDL_Surface *bmp = SDL_LoadBMP("test.bmp");
+	if (bmp == nullptr) {
+		std::cout << "SDL_LoadBMP Error: " << SDL_GetError() << std::endl;
+		// XXX: Proper cleanup
+		return 1;
+	}
+
+	// Create texture, free surface
+	SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, bmp);
+	SDL_FreeSurface(bmp);
+	if (tex == nullptr) {
+		std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
+		// XXX: Proper cleanup
+		return 1;
+	}
+
+	// Draw texture
+	SDL_RenderClear(ren);
+	SDL_RenderCopy(ren, tex, NULL, NULL);
+	SDL_RenderPresent(ren);
+	SDL_Delay(2000);
+
+	SDL_DestroyTexture(tex);
+	SDL_DestroyRenderer(ren);
+	SDL_DestroyWindow(win);
+	SDL_Quit();
+
 	return 0;
 }
